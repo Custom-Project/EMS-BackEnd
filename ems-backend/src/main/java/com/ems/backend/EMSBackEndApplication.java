2b3fbf058ff69d23d5a61062327652f99417b540
FSOFT.FPT.VN\SonVHH <sonvhhhe141043@fpt.edu.vn>
2021-09-13 22:29:32 +0700

    first commit

---
+++ ems-backend/src/main/java/com/ems/backend/EMSBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.ems.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EMSBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EMSBackEndApplication.class, args);
+    }
+
+}
---
+++ ems-backend/src/main/java/com/ems/backend/entity/BaseEntity.java
@@ -0,0 +1,18 @@
+package com.ems.backend.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class BaseEntity {
+
+    @Id
+    private Long id;
+}
